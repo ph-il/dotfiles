279f9b6774964895ebcc0f5d591c8d8300e1bba4
Philippe Gamache <philippe@ex-situ.com>
2018-04-17 11:22:22 -0400

    Update

--- exakat_install.sh
+++ exakat_install.sh
@@ -13,8 +13,6 @@ cd "${PRJ_DIR}/tools/exakat"
 
 curl -o exakat.phar http://dist.exakat.io/index.php?file=latest
 curl -o apache-tinkerpop-gremlin-server-3.2.7-bin.zip http://ftp.tudelft.nl/apache/tinkerpop/3.2.7/apache-tinkerpop-gremlin-server-3.2.7-bin.zip
-
-curl -o apache-tinkerpop-gremlin-server-3.2.6-bin.zip http://ftp.tudelft.nl/apache/tinkerpop/3.2.6/apache-tinkerpop-gremlin-server-3.2.6-bin.zip
 unzip apache-tinkerpop-gremlin-server-3.2.7-bin.zip
 mv apache-tinkerpop-gremlin-server-3.2.7 tinkergraph
 rm -rf apache-tinkerpop-gremlin-server-3.2.7-bin.zip
@@ -32,3 +30,4 @@ sudo sed -i -e "s/;php56        = \/path\/to\/php56/php56 = \/usr\/local\/opt\/p
 sudo sed -i -e "s/;php70        = \/path\/to\/php70/php70 = \/usr\/local\/opt\/php70\/bin\/php/g" config/exakat.ini
 sudo sed -i -e "s/;php71        = \/path\/to\/php71/php71 = \/usr\/local\/opt\/php71\/bin\/php/g" config/exakat.ini
 sudo sed -i -e "s/;php72        = \/path\/to\/php72/php72 = \/usr\/local\/opt\/php72\/bin\/php/g" config/exakat.ini
+
